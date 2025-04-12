c1632874405a88644d85f986acb012d4ab9c6ec4
munergenis <munergenis@gmail.com>
2025-04-12 11:51:21 +0200

    feat: add CORS support

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,15 @@
-import express from "express";
-import cowRouter from "./cow/router";
-import sheepRouter from "./sheep/router";
+import express from 'express';
+import cowRouter from './cow/router';
+import sheepRouter from './sheep/router';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
 // middlewares
 app.use(express.json());
 
-app.use("/api/cows", cowRouter);
-app.use("/api/sheeps", sheepRouter);
+app.use('/api/cows', cowRouter);
+app.use('/api/sheeps', sheepRouter);
 
 export default app;
